a0543fe613c6d2d2b667ea76c2a7d52edbbe01ca
Calum Bradley <cal@gmx.co.uk>
2021-08-27 15:28:45 +0100

    prettier formatted strings to double quotes and a working version of stripe api for variable prices

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({path: '../.env'});
+require("dotenv").config({ path: "../.env" });
 const express = require("express");
 const app = express();
 const { resolve } = require("path");
@@ -7,7 +7,7 @@ const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-app.use(express.static('../client'))
+app.use(express.static("../client"));
 // Use JSON parser for all non-webhook routes
 app.use((req, res, next) => {
   if (req.originalUrl === "/webhook") {
@@ -39,25 +39,29 @@ let port = process.env.PORT;
 
 //Working version
 
-app.post('/create-checkout-session', async (req, res) => {
+app.post("/create-checkout-session", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
     line_items: [
       {
-        // TODO: replace this with the `price` of the product you want to sell
-        price: 'price_1JQzhbHegBXMg0ahcQVD9DBI',
+        price_data: {
+          currency: "gbp",
+          unit_amount: 2000,
+          product_data: {
+            name: "T-shirt",
+            // description: "", cake it or leave it description
+            // images: [], cake it or leave it logo
+          },
+        },
         quantity: 1,
       },
     ],
-    payment_method_types: [
-      'card',
-    ],
-    mode: 'payment',
+    mode: "payment",
     success_url: `http://localhost/success.html`,
     cancel_url: `http://localhost/cancel.html`,
-  })
-  res.redirect(303, session.url)
-
-})
+  });
+  res.redirect(303, session.url);
+});
 
 // CURL -X POST http://localhost:4242/create-checkout-session -D '{"quantity": 1}' -H "Content-Type: application/json"
 
