e1d56da43bb2bac2e99ef18a54c4c5fc722fe625
Calum Bradley <cal@gmx.co.uk>
2021-11-02 22:21:58 +0000

    working version with price in the create-checkout-session route although only works then form submisson is prevented

--- server/server.js
+++ server/server.js
@@ -4,8 +4,12 @@ const app = express();
 const { resolve } = require("path");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { json } = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("../client"));
 // Use JSON parser for all non-webhook routes
@@ -21,7 +25,7 @@ let port = process.env.PORT;
 
 //Working version
 
-const price = 5795;
+//const price = 5795;
 
 // CURL -X POST http://localhost:4242/create-checkout-session -D '{"quantity": 1}' -H "Content-Type: application/json"
 
@@ -50,36 +54,18 @@ app.post(
   }
 );
 
-app.post("/data", (req, res) => {
-  // res.send(console.log('Received POST request from CLIENT'))
-  //Database
-  // const database = new Datastore('./database/orders.db')
-  // database.loadDatabase()
-  // database.insert(req.body);
-  console.log("Added cake order to the database");
-
-  //console.log(req.body[0]);
-
-  //Accessing array then the object property of price
-  let price = req.body[0].price;
-  // //Logging the price
-  console.log(`Cake price is: £${price}`);
-
-  //Price is a number
-  console.log(typeof price);
-
-  //Req - Represents a resource request.
-  //Res - Represents the response to a request.
-});
-
 app.post("/create-checkout-session", async (req, res) => {
+
+  let data = req.body[0].price
+  console.log(data);
+  
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
       {
         price_data: {
           currency: "gbp",
-          unit_amount: price,
+          unit_amount: 5795,
           product_data: {
             name: "CakeItOrLeaveIt",
             // description: "", cake it or leave it description
@@ -93,7 +79,18 @@ app.post("/create-checkout-session", async (req, res) => {
     success_url: `http://localhost:4242/success.html`,
     cancel_url: `http://localhost:4242/cancel.html`,
   });
+
   res.redirect(303, session.url);
 });
+
+
+// app.post('/data', (req, res) => {
+//   console.log(req.body[0].UniqueId);
+//   console.log(req.body[0].price);
+//   res.send({message : "Successful request"})
+// });
+
+
+
 
 app.listen(4242, () => console.log(`Node server listening on port ${port}!`));
