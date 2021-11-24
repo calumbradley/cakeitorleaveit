d844d58f2d5e5b8dd78e45a93ff174855bead13d
Calum Bradley <cal@gmx.co.uk>
2021-11-24 17:02:59 +0000

    removing comments

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ let port = process.env.PORT;
 // CURL -X POST http://localhost:4242/create-checkout-session -D '{"quantity": 1}' -H "Content-Type: application/json"
 
 app.post("/create-checkout-session", async (req, res) => {
+
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     line_items: [
@@ -21,11 +22,8 @@ app.post("/create-checkout-session", async (req, res) => {
         price_data: {
           currency: "gbp",
           unit_amount: 1000,
-          //errors with "TypeError: Cannot read properties of undefined (reading 'price')"
           product_data: {
             name: "CakeItOrLeaveIt",
-            // description: "", cake it or leave it description
-            // images: [], cake it or leave it logo
           },
         },
         quantity: 1,
@@ -35,8 +33,6 @@ app.post("/create-checkout-session", async (req, res) => {
     success_url: `http://localhost:4242/success.html`,
     cancel_url: `http://localhost:4242/cancel.html`,
   });
-
-  // console.log(req.body[0]);
   
   console.log(session.url, session.line_items)
   res.redirect(303, session.url);
