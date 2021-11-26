b89508f3f53b9e8373ca5dcfe350beca2c18977f
Calum Bradley <cal@gmx.co.uk>
2021-11-26 15:31:31 +0000

    updating server with comments for session storage and a static var for price testing and switching to a test route for testing

--- server/server.js
+++ server/server.js
@@ -4,16 +4,21 @@ const app = express();
 const { resolve } = require("path");
 const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const session = require('express-session')
 
+app.use(express.urlencoded({extended : true}));
 app.use(express.json()); //Used to parse JSON bodies
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static("../client"));
 
-let port = process.env.PORT;
+let port = process.env.PORT
 
 // CURL -X POST http://localhost:4242/create-checkout-session -D '{"quantity": 1}' -H "Content-Type: application/json"
 
+let cakePrice = 1000;
+
 app.post("/create-checkout-session", async (req, res) => {
+
+  console.log(req.body)
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
@@ -21,7 +26,7 @@ app.post("/create-checkout-session", async (req, res) => {
       {
         price_data: {
           currency: "gbp",
-          unit_amount: 1000,
+          unit_amount: cakePrice,
           product_data: {
             name: "CakeItOrLeaveIt",
           },
@@ -32,10 +37,16 @@ app.post("/create-checkout-session", async (req, res) => {
     mode: "payment",
     success_url: `http://localhost:4242/success.html`,
     cancel_url: `http://localhost:4242/cancel.html`,
-  });
-  
-  console.log(session.url, session.line_items)
-  res.redirect(303, session.url);
-});
+  })
 
-app.listen(4242, () => console.log(`Node server listening on port ${port}!`));
+  res.redirect(303, session.url)
+})
+
+//Testing Route
+
+app.post('/test', (req, res)=> {
+  console.log(req.body)
+  res.send('POST request')
+})
+
+app.listen(4242, () => console.log(`Node server listening on port ${port}!`))
