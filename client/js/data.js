da12efc87083dd950903ab2c67f1f44275932f5a
Calum Bradley <cal@gmx.co.uk>
2021-12-09 20:45:41 +0000

    removing loads of stuff not needed while the solution isnt working

--- client/js/data.js
+++ client/js/data.js
@@ -1,43 +1,11 @@
-const addData = (ev) => {
+const sendData = (ev) => {
   //ev.preventDefault();
-  //console.log('Sucessfully prevented form submission');
-  //console.log('Creating data object');
-
-  // let cakeShape = document.getElementsByName("cakeShape");
-  // for (let cake of cakeShape) {
-  //   if (cake.checked) {
-  //     //console.log(cake.checked);
-  //     selCakeShape = cake.value;
-  //     break;
-  //   } else selCakeShape = "";
-  // }
-
-  // if (icingSugar() === 5) {
-  //   icing = true;
-  // } else {
-  //   icing = false;
-  // }
 
   let data = {
-    // UniqueId: Date.now(),
-    // cakeSize: parseInt(document.getElementById("cakeSize").value),
-    // cakeShape: selCakeShape,
-    // sponge: document.getElementById("sponge").value,
-    // butterCream: document.getElementById("buttercream").value,
-    // icing: icing,
-    // specialDecoration: document.querySelector("#decorationRequirements").value,
-    // cakeMessage: document.querySelector("#specifyCakeMessage").value,
-    // dateTime: document.querySelector("#dateAndTimeReq").value,
-    price: calculateTotal() * 1000,
+   price: calculateTotal() * 1000
   };
-  //console.log(`Customers unique ID is : ${data.id}`);
-  //console.log(`Customers name is : ${data.name}`);
-  console.log(data.price);
 
-  localStorage.setItem("cakeData", JSON.stringify(data));
-
-  //add back to force post the form
-  //document.querySelector('form').submit()
+  // localStorage.setItem("cakeData", JSON.stringify(data));
 
   let options = {
     method: "POST",
@@ -47,18 +15,31 @@ const addData = (ev) => {
     body: JSON.stringify(data),
   };
 
-  delete data.price;
 
-  fetch("/create-checkout-session", options)
-    .then((data) => {
-      console.log("Success:", data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+        fetch("/pay", options)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (session) {
+            return stripe.redirectToCheckout({ sessionId: session.id });
+          })
+          .then(function (result) {
+            // If redirectToCheckout fails due to a browser or network
+            // error, you should display the localized error message to your
+            // customer using error.message.
+            if (result.error) {
+              alert(result.error.message);
+            }
+          })
+          .catch(function (error) {
+            console.error("Error:", error);
+          });
 
-  //document.querySelector('form').reset()
-  //document.querySelector('form').submit
-};
+          delete data.price;
 
-document.querySelector("#submitButton").addEventListener("click", addData);
+      }
+
+
+
+ document.querySelector("#submitButton").addEventListener("click", sendData);
+
