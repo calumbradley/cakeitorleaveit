cdab6209e1a57361096749e5f8b241f1acc8b866
Calum Bradley <cal@gmx.co.uk>
2021-12-09 20:46:32 +0000

    removing lots of used code

--- server/server.js
+++ server/server.js
@@ -4,47 +4,70 @@ const app = express();
 const { resolve } = require("path");
 const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const session = require('express-session')
+// const session = require('express-session')
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.static("../client"));
-app.use(session({ secret: 'some session', cookie: { maxAge: 60000 }}))
+// app.use(session({ secret: 'some session', cookie: { maxAge: 60000 }}))
 
 
 let port = process.env.PORT;
 
-// CURL -X POST http://localhost:4242/create-checkout-session -D '{"quantity": 1}' -H "Content-Type: application/json"
+// app.post("/create-checkout-session", async (req, res) => {
+ 
+// console.log(req.body);
 
-app.post("/create-checkout-session", async (req, res) => {
+//    const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ["card"],
+//     line_items: [
+//       {
+//         "price_data": {
+//           "currency": "gbp",
+//           "product_data": {
+//             "name": "test"
+//           },
+//           "unit_amount": 1000
+//         },
+//         "quantity": 1
+//       },
+//     ],
+//     mode: "payment",
+//     success_url: `http://localhost:4242/success.html`,
+//     cancel_url: `http://localhost:4242/cancel.html`,
+//   });
 
-   let session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "gbp",
-          unit_amount: 1000,
-          product_data: {
-            name: "CakeItOrLeaveIt",
-          },
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `http://localhost:4242/success.html`,
-    cancel_url: `http://localhost:4242/cancel.html`,
-  });
+//   res.redirect(303, session.url);
 
-  res.redirect(303, session.url);
-});
+
+// });
 
 //Testing Route
 
-// app.post("/test", (req, res) => {
-//   console.log(req.body.price);
-//   res.send("POST request");
-// });
+app.post("/pay", async (req, res) => {
+  const { product } = req.body;
+  const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+            price_data: {
+                currency: "gbp",
+                product_data: {
+                    name: 'test',
+                },
+                unit_amount: 1000,
+            },
+            quantity: 1,
+        },
+    ],
+      mode: "payment",
+      success_url: `http://localhost:4242/success.html`,
+      cancel_url: `http://localhost:4242/cancel.html`
+  });
+
+  res.json({ id: session.id });
+});
 
 app.listen(4242, () => console.log(`Node server listening on port ${port}!`));
+
+
